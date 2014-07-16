accc0d6135732f647ad67253607f1f85bb5d09df
CJ Carey <perimosocordiae@gmail.com>
2014-07-16 17:05:26 -0400

    adding basic linux support

--- talkback.sh
+++ talkback.sh
@@ -1,17 +1,31 @@
 # talkback.sh - Have your computer read back the commands you run.
 # Usage: source talkback.sh
 
-# TODO: set this more intelligently for non-Mac systems.
-text_to_speech='say'
+# Set this if you know which TTS program you want.
+DEFAULT_TTS=
+
+if [ -n "$DEFAULT_TTS" ]; then
+  text_to_speech="$DEFAULT_TTS"
+elif [ "$(uname)" == 'Linux' ]; then
+  text_to_speech='espeak'
+else
+  text_to_speech='say'
+fi
+
+SED='sed'
+# Use 'sed -r' if we're using GNU sed.
+$SED -re '' <<<'' && SED='sed -r'
 
 # Function that runs before the command is executed.
 # Add more -e replacements to the sed call as needed.
 preexec () {
-  cmd=`sed -e 's/\./ dot /g' \
-           -e 's/\|/ pipe /g' \
-           -e 's/\*/ star /g' \
-           <<<$@`
-  sh -c "$text_to_speech \"$cmd\" &" 2>/dev/null;
+  cmd=$($SED -e 's/\./ dot /g' \
+             -e 's/\|/ pipe /g' \
+             -e 's/\*/ star /g' \
+             -e 's/"//g' \
+             <<<"$@")
+  echo "$text_to_speech \"$cmd\""
+  # sh -c "$text_to_speech \"$cmd\" &" 2>/dev/null;
 }
 
 # Idea borrowed from http://superuser.com/a/175802/74234
@@ -23,7 +37,7 @@ preexec_invoke_exec () {
   # get the previous command from history
   # Note: this isn't always the last command executed,
   # depeding on the user's history configuration.
-  local this_command=`history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//"`;
+  local this_command=$(history 1 | $SED -e "s/^[ ]*[0-9]*[ ]*//");
   preexec "$this_command"
 }
 
